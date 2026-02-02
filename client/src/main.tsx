228558576dfd72b96f66009ce4795ee31c31cab0
Tia Noel <tiagolson@gmail.com>
2026-02-02 10:36:04 -0500

    updates main tsx after creating the favorites file

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,14 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { FavoritesProvider } from "./context/FavoritesContext";
 import "./styles/global.css";
 
-// Note to self: Wrap the app with BrowserRouter so routing works everywhere.
+// Note to self: Provide favorites globally for all routes.
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <FavoritesProvider>
+        <App />
+      </FavoritesProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
-
