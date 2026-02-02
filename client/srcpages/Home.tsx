e92cb54d2c881deab846aafc00e849e6eafc59ec
Tia Noel <tiagolson@gmail.com>
2026-02-02 11:42:30 -0500

    Home Page added

---
+++ client/srcpages/Home.tsx
@@ -0,0 +1,32 @@
+import { useFetch } from "../hooks/useFetch";
+import Spinner from "../components/Spinner";
+import ErrorMessage from "../components/ErrorMessage";
+import { Link } from "react-router-dom";
+
+type CategoriesResponse = {
+  categories: { idCategory: string; strCategory: string; strCategoryThumb: string }[];
+};
+
+// Note to self: Home shows all categories.
+export default function Home() {
+  const { data, loading, error } = useFetch<CategoriesResponse>(
+    "https://www.themealdb.com/api/json/v1/1/categories.php"
+  );
+
+  if (loading) return <Spinner />;
+  if (error) return <ErrorMessage message={error} />;
+
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Categories</h1>
+      <div style={{ display: "grid", gap: "1rem", gridTemplateColumns: "repeat(auto-fit, minmax(180px, 1fr))" }}>
+        {data?.categories.map((cat) => (
+          <Link key={cat.idCategory} to={`/category/${cat.strCategory}`}>
+            <img src={cat.strCategoryThumb} alt={cat.strCategory} />
+            <p>{cat.strCategory}</p>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+}
