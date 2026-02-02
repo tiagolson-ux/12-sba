295b7497789aa6941616e089aacae6684eaebada
Tia Noel <tiagolson@gmail.com>
2026-02-02 10:36:54 -0500

    feat: wrap app with FavoritesProvider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,7 +5,7 @@ import App from "./App";
 import { FavoritesProvider } from "./context/FavoritesContext";
 import "./styles/global.css";
 
-// Note to self: Provide favorites globally for all routes.
+// Note to self: Provide favorites globally 4 all routes.
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
